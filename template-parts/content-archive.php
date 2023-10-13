d7c3eaa75243c1011c69d7026a83cc5460ef410c
SafeCo <ahmed01sa@yahoo.co.uk>
2023-10-13 12:19:57 +0100

    made post title into links

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,5 +1,32 @@
+<div>content archive.php working</div>
+<div>
+    <!-- black border added in style.css -->
+    <img class="testing" src="<?php the_post_thumbnail_url('thumbnail'); ?>" />
+</div>
 
-<div>content-archive.php is working</div>
+<div>
+    <a href="<?php the_permalink(); ?>" >
+        <?php
+            the_title();
+        ?>
+    </a>
+</div>
+<div>
+    <?php
+        the_date();
+    ?>
+</div>
+
+<div>
+    <?php
+        comments_number();
+    ?>
+</div>
+
 <?php
-    the_content();
+    the_excerpt();
 ?>
+
+<h2>
+    <a href="<?php the_permalink(); ?>" >Read more</a>
+</h2>
---
+++ template-parts/content-page.php
@@ -0,0 +1,5 @@
+<div>content-page.php working</div>
+
+<?php
+    the_content();
+?>
