c87f836ea7555b31cabd57c16fe32139ec02f8da
SafeCo <ahmed01sa@yahoo.co.uk>
2023-10-29 16:16:18 +0000

    added further styles to footer

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
         <footer class="page-footer">
             <div class="footer-container">
 
-                <div class="footer-col1 tempborder <?php disable_widget("footer-2")?> ">
+                <div class="footer-col1 <?php disable_widget("footer-2")?> ">
                     <!-- Search bar -->
                     <?php
                         dynamic_sidebar('footer-2')
@@ -29,18 +29,20 @@
                     ?> 
                 </ul>
 
-                <div class="footer-col4 tempborder">
-                <div>Join our newsletter!</div>
-                <form id="footer-form">
-                    <input class="form-elements" placeholder="Name" type="text" name="name">
-                    <input class="form-elements" placeholder="Email" type="text" name="email">
-                    <input id="form-submit"  type="submit" value="Go">
-                </form>
+                <div class="footer-col4 ">
+                    <p>Join our newsletter!</p>
+                    <form id="footer-form">
+                        <input class="form-elements" placeholder="Name" type="text" name="name">
+                        <input class="form-elements" placeholder="Email" type="text" name="email">
+                        <input id="form-submit"  type="submit" value="Go">
+                    </form>
                 </div>
             </div>
             
-            <div class="footer-cc tempborder">
+            <div class="footer-cc">
                 <p>© The Food Reviewer 2023. Privacy Policy.</p>
+                <i class="bi bi-instagram"></i>
+
             </div>
             
         </footer>
